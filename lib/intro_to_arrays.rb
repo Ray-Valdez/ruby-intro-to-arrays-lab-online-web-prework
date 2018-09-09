f744f517156ffd86283c38906d034bf082047e55
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-09 20:33:39 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,8 +1,8 @@
-def instantiate_new_array(my_new_array)
+def instantiate_new_array
   []
 end
 
-def array_with_two_elements(my_two_array)
+def array_with_two_elements
   my_two_array = [1,2]
   my_two_array
 end
